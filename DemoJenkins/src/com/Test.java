60ebc1cfaf28ef4eac7a57e7d18b4b8451bdfc9a
manjiri-1998 <manjirikshirsagar98@gmail.com>
2026-04-20 17:30:58 +0530

    By Amit

--- DemoJenkins/src/com/Test.java
+++ DemoJenkins/src/com/Test.java
@@ -7,4 +7,8 @@ public class Test
 		System.out.println("This is the main method");
 		System.out.println("This is the main2");
 	}
+	public void m1() {
+		System.out.println("This is the m1 method");
+
+	}
 }
